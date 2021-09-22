72d9b0e03d7be8e08b0a27f104afdf2d5c2efa71
Sören Beye <github@hypfer.de>
2021-09-22 23:44:46 +0200

    fix: Fix concurrent .resolveService

--- app/src/main/java/cloud/valetudo/companion/MainActivity.kt
+++ app/src/main/java/cloud/valetudo/companion/MainActivity.kt
@@ -11,16 +11,19 @@ import android.content.Intent
 import android.net.Uri
 import android.view.View
 import android.widget.*
+import java.util.concurrent.Semaphore
 import kotlin.collections.HashMap
+import kotlin.concurrent.thread
 
 
 class MainActivity : AppCompatActivity() {
-    private val TAG = "me.hackerchick.valetudo"
+    private val TAG = "cloud.valetudo"
 
     private var mNsdManager : NsdManager? = null
 
     private var mValetudoDevices = HashMap<String, String>()
     private var mValetudoDeviceDescriptions = ArrayList<String>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,36 +37,78 @@ class MainActivity : AppCompatActivity() {
         discoveredList.adapter = itemsAdapter
         discoveredList.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
             val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://${mValetudoDevices[mValetudoDeviceDescriptions[position]]}"))
+
             startActivity(browserIntent)
         }
 
-        mNsdManager = (getSystemService(Context.NSD_SERVICE) as NsdManager)
-        mNsdManager!!.discoverServices("_valetudo._tcp.", NsdManager.PROTOCOL_DNS_SD, object : NsdManager.DiscoveryListener {
-            override fun onStartDiscoveryFailed(serviceType: String?, errorCode: Int) {}
-            override fun onStopDiscoveryFailed(serviceType: String?, errorCode: Int) {}
-            override fun onDiscoveryStarted(serviceType: String?) {
-                Log.d(TAG, "Service discovery started")
-            }
-            override fun onDiscoveryStopped(serviceType: String?) {}
-            override fun onServiceFound(serviceInfo: NsdServiceInfo?) {
-                Log.d(TAG, "Service discovery success$serviceInfo")
+        val resolveSemaphore = Semaphore(1)
+        fun tryResolve(serviceInfo: NsdServiceInfo) {
+            thread {
+                resolveSemaphore.acquire()
+
                 mNsdManager!!.resolveService(serviceInfo, object : NsdManager.ResolveListener {
-                    override fun onResolveFailed(serviceInfo: NsdServiceInfo?, errorCode: Int) {}
+                    override fun onResolveFailed(serviceInfo: NsdServiceInfo?, errorCode: Int) {
+                        resolveSemaphore.release();
+
+                        Log.d(TAG, "Service resolve failed $serviceInfo Error Code: $errorCode")
+                    }
+
                     override fun onServiceResolved(serviceInfo: NsdServiceInfo?) {
+                        resolveSemaphore.release()
+
                         runOnUiThread {
                             mainText.visibility = View.GONE
                             listLayout.visibility = View.VISIBLE
                         }
 
-                        Log.d(TAG, "Service resolve success$serviceInfo")
+                        Log.d(TAG, "Service resolve success $serviceInfo")
 
-                        val deviceDescription = "${serviceInfo!!.serviceName} (${serviceInfo.host.hostAddress})"
+                        var deviceLabel = serviceInfo!!.serviceName
+
+                        val vendor = serviceInfo.attributes.get("manufacturer");
+                        val model = serviceInfo.attributes.get("model");
+
+                        if(vendor != null && model != null) {
+                            deviceLabel = "${String(vendor)} ${String(model)}"
+                        }
+
+
+                        val deviceDescription = "$deviceLabel (${serviceInfo.host.hostAddress})"
                         mValetudoDevices[deviceDescription] = serviceInfo.host.hostAddress
                         mValetudoDeviceDescriptions.add(deviceDescription)
                     }
                 })
             }
-            override fun onServiceLost(serviceInfo: NsdServiceInfo?) {}
+        }
+
+
+        mNsdManager = (getSystemService(Context.NSD_SERVICE) as NsdManager)
+
+        mNsdManager!!.discoverServices("_valetudo._tcp.", NsdManager.PROTOCOL_DNS_SD, object : NsdManager.DiscoveryListener {
+            override fun onStartDiscoveryFailed(serviceType: String?, errorCode: Int) {}
+
+            override fun onStopDiscoveryFailed(serviceType: String?, errorCode: Int) {}
+
+            override fun onDiscoveryStarted(serviceType: String?) {
+                Log.d(TAG, "Service discovery started")
+            }
+
+            override fun onDiscoveryStopped(serviceType: String?) {}
+
+            override fun onServiceFound(serviceInfo: NsdServiceInfo?) {
+                Log.d(TAG, "Service discovery success $serviceInfo")
+
+                if (serviceInfo != null) {
+                    tryResolve(serviceInfo);
+                } else {
+                    Log.d(TAG, "ServiceInfo is null")
+                }
+
+            }
+
+            override fun onServiceLost(serviceInfo: NsdServiceInfo?) {
+                Log.d(TAG, "Service lost $serviceInfo")
+            }
         })
     }
 }
